fd5cef9085261a76af37462c829059c236efcab4
MedvedevOO <bearzoleg@gmail.com>
2024-09-26 02:57:35 +0400

    updates

--- app/src/main/java/com/example/musicplayer/domain/usecase/GetAlbumByIdUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.musicplayer.domain.usecase
-
-import com.example.musicplayer.domain.repository.MusicRepository
-import javax.inject.Inject
-
-class GetAlbumByIdUseCase @Inject constructor (private val musicRepository: MusicRepository) {
-    operator fun invoke(id: Int) = musicRepository.getAlbumById(id)
-}
--- app/src/main/java/com/example/musicplayer/domain/usecase/GetArtistIdByNameUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.musicplayer.domain.usecase
-
-import com.example.musicplayer.domain.repository.MusicRepository
-import javax.inject.Inject
-
-class GetArtistIdByNameUseCase @Inject constructor (private val musicRepository: MusicRepository) {
-    operator fun invoke(name: String) = musicRepository.getArtistIdByName(name)
-}
